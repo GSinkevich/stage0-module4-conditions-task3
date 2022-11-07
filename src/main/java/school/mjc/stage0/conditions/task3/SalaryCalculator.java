48ebaccada57d6f5deb766972cfa50db8bf275e4
DESKTOP-GV962H6\sinke <sinkevich.work@gmail.com>
2022-11-07 15:58:52 +0300

    task done

--- src/main/java/school/mjc/stage0/conditions/task3/SalaryCalculator.java
+++ src/main/java/school/mjc/stage0/conditions/task3/SalaryCalculator.java
@@ -2,5 +2,21 @@ package school.mjc.stage0.conditions.task3;
 
 public class SalaryCalculator {
     public void calculateSalary(int salary) {
+        double result = 0;
+                if (salary < 0){
+                    System.out.println("wrong input!");
+                    return;
+                }
+                else if (salary > 20000){
+                    result = salary * 0.8;}
+                else if (salary <= 10000){
+                    result = salary * 0.85;
+                    }
+                else if(salary <= 20000){
+                    result = salary * 0.82;
+                }
+
+
+        System.out.println(result);
     }
 }
--- src/main/java/school/mjc/stage0/conditions/task3/Seasons.java
+++ src/main/java/school/mjc/stage0/conditions/task3/Seasons.java
@@ -2,6 +2,20 @@ package school.mjc.stage0.conditions.task3;
 
 public class Seasons {
     public void tellTheSeasonByMonthNumber(int month) {
-
+        if (month > 13 || month <= 0) {
+            System.out.println("wrong number!");
+        }
+        else if (month >= 6 && month <= 8) {
+            System.out.println("Summer");
+        }
+        else if (month == 12 ||  month <= 2) {
+            System.out.println("Winter");
+        }
+        else if (month >= 3 & month <= 5) {
+            System.out.println("Spring");
+        }
+        else {
+            System.out.println("Autumn");
+        }
     }
 }
